2899801d5802728c568fdf7ecb90e803b11feb62
Nakyung Kim <Nakyung2371@gmail.com>
2023-12-27 18:30:21 +0900

    12.27. logout

--- Oracle Script/board.sql
+++ Oracle Script/board.sql
@@ -47,5 +47,22 @@ commit;
 drop table board;
 desc board;
 
-select * from board order by seq desc
+select * from board order by seq desc;
+
+update board
+set title = '제목 수정됨', write = '이순신', content = '내용 수정됨'
+where seq = 4;
+
+commit;
+rollback;
+delete board where seq = 3;
+
+update board
+set cnt = cnt + 1
+where seq = 8;
+
+desc users;
+
+select * from users
+where id = 'user' and password = '1234';
 
